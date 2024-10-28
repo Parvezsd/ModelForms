2ec8341aae4960b57babd49bb100be37d2914577
Parvezsd <syedparvez5577@gmail.com.com>
2024-10-28 17:36:04 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -8,10 +8,21 @@ class TopicModelForms(forms.ModelForm):
 class WebpageModelForms(forms.ModelForm):
     class Meta:
         model = Webpage
-        fields ='__all__'
+        # fields ='__all__'
+        # fields=['topic_name','name']
+        exclude=['email']
+        labels={'topic_name':'TN'}
+        widgets={'url':forms.PasswordInput}
+        help_texts={'topic_name':'This is parent data'}
 
 class AccessRecordModelForms(forms.ModelForm):
     class Meta:
         model = AccessRecord
         fields ='__all__'
+        # fields=['name','author']
+        # exclude=['author']
+        labels={'author':'A','date':'D','name':'N'}
+        widgets={'date':forms.PasswordInput}
+        help_texts={'name':'this is parent data'}
+
         
